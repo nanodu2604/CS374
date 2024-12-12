3fa12dfcdb96b6e5af69969f17c040f3b2c228ed
nanodu2604 <98722763+nanodu2604@users.noreply.github.com>
2024-12-12 00:39:34 +0000

    Update java project

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.println("Enter the interval: ");
+            String interval=scan.nextLine();
+
+        }
+    }
+}
